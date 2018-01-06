c0438b78db7dc9c68fad3faba201b11223dae18f
William McCullough <ZahFoxTales@gmail.com>
2018-01-05 22:08:30 -0600

    Began working on the aiming system for the Player tank. This will be implemented by tracing a raycast from the crosshair location on the UI.

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -16,10 +16,19 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
 
+	virtual void Tick(float DeltaTime) override;
+
+	ATank* GetControlledTank() const;
+
 private:
 
+	// Start the tank moving the barrel so that a shot would hit where the
+	// crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 };
