61f32228376cbe0ad20befd1c5ed15edea7a214e
William McCullough <ZahFoxTales@gmail.com>
2018-02-20 15:29:43 -0600

    Created a class for the TankTracks and bound keyboard and xbox controller input to apply force to their mesh origins.

---
+++ BattleTank/Source/BattleTank/Public/TankTrack.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/StaticMeshComponent.h"
+#include "TankTrack.generated.h"
+
+/**
+ * TankTack is used to set maximum driving force, and to apply force to the tank.
+ */
+UCLASS(meta = (BlueprintSpawnableComponent))
+class BATTLETANK_API UTankTrack : public UStaticMeshComponent
+{
+	GENERATED_BODY()
+	
+public:
+
+	// Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
+
+	// Max force per track, in Newtons
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; // Assume 40 tonne tanks, and 1g accelleration
+	
+	
+};
